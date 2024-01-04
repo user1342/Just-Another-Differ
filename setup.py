a24cc6e7376e46afa84cde2d88e87bcf7230a41a
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-01-04 22:59:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'tweezer=JustAnotherDiffer.JAD:entry',
+            'jad=JustAnotherDiffer.JAD:entry',
         ],
     },
 )
